29336537ad9aaef7d8eea985316b035ffca63712
chiba <ryotai2677@gmail.com>
2024-10-02 13:13:47 +0900

    a

--- src/components/TopAndLoading/TopAndLoading.js
+++ src/components/TopAndLoading/TopAndLoading.js
@@ -1,12 +1,30 @@
-import React from "react";
-import * as styles from "./Top.module.css";
+import React, { useEffect, useState } from "react";
+import * as styles from "./TopAndLoading.module.css";
 
 import TopSmallBox from "../TopSmallBox/TopSmallBox";
 import AnimatedElement from "../AnimatedElement";
 
 const Top = () => {
+    const [isLoading, setIsLoading] = useState(true)
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+        setIsLoading(false)
+        }, 3000) 
+
+        return () => clearTimeout(timer)
+    }, [])
+    
     return (
         <div className={styles.Top}>
+            <div className={`${styles.loadingScreen} ${!isLoading ? styles.loaded : ''}`}>
+                <img src="background/OxfordBlue.png" alt="OxfordBlue" className={styles.loadingBackground} />
+                <div className={styles.loadingContent}>
+                    <img src="logo/loading_title_2.png" alt="Loading_2" className={styles.loadingImage} />
+                    <img src="logo/loading_title_1.png" alt="Loading_1" className={styles.loadingImage} />
+                    
+                </div>
+            </div>
            <img src="/top/background.png" alt="background_top" className={styles.Background}/>
            <div className={styles.TextContent}>
                 <h1 className={styles.Title}>
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,17 +2,17 @@ import React from "react";
 import Layout from "../components/layout";
 
 import Top from "../components/Top/Top";
+import TopAndLoading from "../components/TopAndLoading/TopAndLoading";
 import Feature from "../components/Feature/Feature";
 
 import Service from "../components/Service/Service";
 import Work from "../components/Work/Work";
 import Contact from "../components/Contact/Contact";
-import LoadingScreen from "../components/LoadingScreen/LoadingScreen";
 
 const Index = () => {
     return (
         <Layout>
-            <LoadingScreen />
+            <TopAndLoading />
             <Top />
             <Feature />
             <Work />
