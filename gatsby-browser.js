dc110888d6a4492854f67dc81ce363593967d9bf
chiba <ryotai2677@gmail.com>
2024-11-23 14:39:53 +0900

    aa

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,1 +1,14 @@
 import "./global.css"
+import { Amplify } from 'aws-amplify';
+import awsconfig from './aws-exports';
+
+Amplify.configure({
+  ...awsconfig,
+  API: {
+    ...awsconfig.API,
+    endpoints: [{
+      name: 'contactapi',
+      endpoint: process.env.GATSBY_API_ENDPOINT
+    }]
+  }
+});
