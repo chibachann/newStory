94c54ecf2a8544fb24fac9975ed2dd159b672006
chiba <ryotai2677@gmail.com>
2024-12-03 16:13:54 +0900

    og image

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,12 +1,12 @@
 module.exports = {
     siteMetadata: {
         title: 'ウェブサイト制作・ホームページ作成 | Story Web Solutions',
-        description: "【東京・千葉】プロフェッショナルなウェブサイト制作・ホームページ制作なら Story Web Solutions。最新技術とデザインで、集客できるウェブサイトを実現。",
+        description: "【東京・千葉・埼玉・神奈川】プロフェッショナルなウェブサイト制作・ホームページ制作なら Story Web Solutions。最新技術とデザインで、集客できるウェブサイトを実現。",
         siteUrl: "https://www.storyweb-solutions.com",
         author: "Story Web Solutions",
         lang: "ja",
         image: "/images/ogp-image.jpg",
-        keywords: "ウェブサイト制作,ホームページ作成,Web制作会社,SEO対策,レスポンシブデザイン,千葉,東京",
+        keywords: "ウェブサイト制作,ホームページ作成,Web制作会社,SEO対策,レスポンシブデザイン,千葉,東京,埼玉,神奈川",
         address: {
             streetAddress: "渋谷２－１９－１５宮益坂ビルディング６０９",
             addressLocality: "渋谷区",
--- src/pages/contactPage.js
+++ src/pages/contactPage.js
@@ -15,20 +15,36 @@ const Contact = () => {
 export default Contact;
 
 export const Head = () => {
-    const data = useStaticQuery(graphql`
-      query {
-        site {
-          siteMetadata {
-            title
-            description
-          }
+  const data = useStaticQuery(graphql`
+    query {
+      site {
+        siteMetadata {
+          title
+          description
+          siteUrl
         }
       }
-    `)
-    return (
-      <>
-       <title>{data.site.siteMetadata.title}</title>
-       <meta name="description" content={data.site.siteMetadata.description} />
-      </>
-    );
-  }
+    }
+  `)
+  return (
+    <>
+      <title>{data.site.siteMetadata.title}</title>
+      <meta name="description" content={data.site.siteMetadata.description} />
+      
+      {/* OGタグ */}
+      <meta property="og:title" content={data.site.siteMetadata.title} />
+      <meta property="og:description" content={data.site.siteMetadata.description} />
+      <meta property="og:type" content="website" />
+      <meta property="og:url" content={data.site.siteMetadata.siteUrl} />
+      <meta property="og:image" content={`${data.site.siteMetadata.siteUrl}/OG.jpg`} />
+      <meta property="og:image:width" content="1200" />
+      <meta property="og:image:height" content="630" />
+      
+      {/* Twitterカード */}
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:title" content={data.site.siteMetadata.title} />
+      <meta name="twitter:description" content={data.site.siteMetadata.description} />
+      <meta name="twitter:image" content={`${data.site.siteMetadata.siteUrl}/OG.jpg`} />
+    </>
+  );
+}
